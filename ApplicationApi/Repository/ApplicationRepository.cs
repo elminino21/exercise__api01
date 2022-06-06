226339ab622628d8c760a50b7ba44d1963079060
Jose gonzalez <joseaugusto978@gmail.com>
2022-06-06 16:29:02 +0900

    fixed query string

--- ApplicationApi/Repository/ApplicationRepository.cs
+++ ApplicationApi/Repository/ApplicationRepository.cs
@@ -44,18 +44,24 @@ namespace ApplicationApi.Repository
             var parameters = new DynamicParameters();
             // this action can be set as a trigger in another type of Db. or on a larger application a utility class can be set and inject to the repo
             categoryname = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(categoryname.ToLower());
-
-            if( toDate == null && fromDate == null)
+            // default query
+            query = "SELECT * FROM Application WHERE BusinessCategory = @BusinessCategory";
+            parameters.Add("@BusinessCategory", categoryname, DbType.String);
+            // search between the two given dates
+            if( toDate != null && fromDate != null)
             {
-                query = "SELECT * FROM Application WHERE BusinessCategory = @BusinessCategory";
-
-            }else{
-
-                query = "SELECT * FROM Application WHERE (DateTimeUpdated BETWEEN @fromDate AND @toDate) AND BusinessCategory = @BusinessCategory";
+                query += " AND (DateTimeUpdated BETWEEN @fromDate AND @toDate)";
                 parameters.Add("@toDate", toDate, DbType.DateTime);
                 parameters.Add("@fromDate", fromDate, DbType.DateTime);
+            // search from the gvien date on wards
+            }else if(fromDate != null){
+                query += " AND DateTimeUpdated >=  @fromDate";
+                parameters.Add("@fromDate", fromDate, DbType.DateTime);
+            // search up to the given date
+            }else if(toDate != null){
+                query += " AND DateTimeUpdated <=  @toDate";
+                parameters.Add("@toDate", fromDate, DbType.DateTime);
             }
-                parameters.Add("@BusinessCategory", categoryname, DbType.String);
 
             using (var connection = _context.StartConnection())
             {
